5b969be94d4b96458bc03c21b1efffdd5ebe9470
Ben Johnson <bkj.322@gmail.com>
2020-01-13 16:21:43 -0500

    cleaning up

--- scale.py
+++ scale.py
@@ -31,7 +31,7 @@ from ppnp.preprocessing import gen_splits, normalize_attributes
 
 from model import EmbeddingPPNP
 from helpers import set_seeds, SimpleEarlyStopping
-from ppr import ExactPPR, DenseNibblePPR, SparseNibblePPR
+from ppr import ExactPPR, DenseNibblePPR, SparseNibblePPR, PrecomputedSparseNibblePPR
 
 def gen_seeds():
     max_uint32 = np.iinfo(np.uint32).max
@@ -39,7 +39,6 @@ def gen_seeds():
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--inpath', type=str, default='ppnp/data/cora_ml.npz')
     parser.add_argument('--n-runs', type=int, default=5)
     parser.add_argument('--seed',   type=int, default=123)
     
@@ -56,16 +55,11 @@ def parse_args():
     parser.add_argument('--ppr-topk',         type=int)
     parser.add_argument('--ppr-mode',         type=str, default='exact')
     
-    parser.add_argument('--batch-size',       type=int, default=256)
+    parser.add_argument('--batch-size',       type=int, default=1024)
     
     parser.add_argument('--verbose', action="store_true")
     
     args = parser.parse_args()
-    
-    is_ms_academic = 'ms_academic' in args.inpath
-    if is_ms_academic:
-        args.alpha  = 0.2
-        args.nknown = 5000
     
     return args
 
@@ -79,19 +73,38 @@ def load():
     data    = np.load('data/youtube/adj_data.npy')
     indices = np.load('data/youtube/adj_indices.npy')
     indptr  = np.load('data/youtube/adj_indptr.npy')
+    mids    = np.load('data/youtube/adj_ids.npy')
     
     n_nodes = indices.max() + 1
-    return sp.csr_matrix((data, indices, indptr), shape=(n_nodes, n_nodes))
+    adj = sp.csr_matrix((data, indices, indptr), shape=(n_nodes, n_nodes))
+    
+    assert len(mids) == n_nodes
+    
+    return adj, mids
 
-adj = load()
+adj, mids = load()
 
-n       = 100_000
-n_train = int(n * 0.9)
+# >>
+# n       = adj.shape[0]
+# n_train = int(n * 0.9)
+# --
 
-adj     = adj[:n, :n]
+n       = 500_000
+adj     = adj[:n,:n]
+
+keep = np.where(adj @ np.ones(adj.shape[0]))[0]
+mids = mids[keep]
+adj  = adj[keep][:,keep]
+
+n_train = int(adj.shape[0] * 0.9)
+
+# <<
 
 adj = sp.eye(adj.shape[0]) + adj
 adj = (adj > 0).astype(np.float32)
+adj = adj.copy()
+
+assert (adj != adj.T).sum() == 0
 
 # --
 #  Define data
@@ -99,23 +112,37 @@ adj = (adj > 0).astype(np.float32)
 X = torch.arange(adj.shape[0]) # Just doing embeddings
 
 t = time()
+# >>
+# indices = torch.load('ppr.indices')
+# values  = torch.load('ppr.values')
+# ppr = PrecomputedSparseNibblePPR(indices=indices, values=values)
+# --
 ppr = SparseNibblePPR(adj=adj, alpha=args.alpha, topk=args.ppr_topk)
+# torch.save((ppr.indices, ppr.values), 'ppr.1138499')
+# <<
+
 ppr_elapsed = time() - t
 
 print('ppr_elapsed', ppr_elapsed, file=sys.stderr)
+# 3.25 hours for whole graph
+# 500s for 224K nodes
 
 model = EmbeddingPPNP(
     n_nodes = adj.shape[0],
     ppr     = ppr
 ).cuda()
 
-opt = torch.optim.Adam(model.parameters(), lr=args.lr)
+opt = torch.optim.Adam(model.parameters(), lr=0.05) # !! Should anneal
 
-idx    = torch.randperm(X.shape[0])
+idx = torch.randperm(X.shape[0])
 idx_train, idx_valid = idx[:n_train], idx[n_train:]
 
+args.batch_size = 1024
 train_loader = DataLoader(TensorDataset(idx_train), batch_size=args.batch_size, shuffle=True, num_workers=0)
-valid_loader = DataLoader(TensorDataset(idx_valid), batch_size=args.batch_size, shuffle=False, num_workers=0)
+valid_loader = list(DataLoader(TensorDataset(idx_valid), batch_size=args.batch_size, shuffle=False, num_workers=0))
+
+for param_group in opt.param_groups:
+        param_group['lr'] *= 0.5
 
 t = time()
 for epoch in range(args.max_epochs):
@@ -127,7 +154,8 @@ for epoch in range(args.max_epochs):
     
     train_loss    = 0
     train_batches = len(train_loader)
-    for idx_batch, in tqdm(train_loader, total=train_batches):
+    gen = tqdm(train_loader, total=train_batches)
+    for idx_batch, in gen:
         node_enc, hood_enc = model(X=X, idx=idx_batch)
         
         loss = ((node_enc - hood_enc) ** 2).mean()
@@ -138,6 +166,8 @@ for epoch in range(args.max_epochs):
         opt.step()
         
         train_loss += loss
+        
+        gen.set_postfix(**{'loss' : 1000 * float(loss)})
     
     train_loss = 1000 * train_loss / train_batches
     
@@ -148,7 +178,8 @@ for epoch in range(args.max_epochs):
     
     valid_loss    = 0
     valid_batches = len(valid_loader)
-    for idx_batch, in tqdm(valid_loader, total=valid_batches):
+    gen = tqdm(valid_loader, total=valid_batches)
+    for idx_batch, in gen:
         node_enc, hood_enc = model(X=X, idx=idx_batch)
         
         loss = ((node_enc - hood_enc) ** 2).mean()
@@ -178,12 +209,13 @@ for epoch in range(args.max_epochs):
 # Train classifier
 
 from sklearn import metrics
-from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_predict, train_test_split
 
+_ = model.eval()
+
 all_encs = []
-idx_all      = torch.arange(adj.shape[0])
+idx_all  = torch.arange(adj.shape[0])
 valid_loader = DataLoader(TensorDataset(idx_all), batch_size=args.batch_size, shuffle=False, num_workers=0)
 for idx_batch, in tqdm(valid_loader, total=len(valid_loader)):
     _, hood_enc = model(X=X, idx=idx_batch)
@@ -192,20 +224,59 @@ for idx_batch, in tqdm(valid_loader, total=len(valid_loader)):
 
 
 all_encs = np.vstack(all_encs)
+all_encs_orig = all_encs.copy()
 all_encs /= np.sqrt((all_encs ** 2).sum(axis=-1, keepdims=True))
 
 labels         = pd.read_csv('/home/bjohnson/.graphvite/dataset/youtube/youtube_label.txt', header=None, sep='\t')
 labels.columns = ('id', 'label')
-labels         = labels[labels.id.isin(idx_all)]
+labels         = labels[labels.id.isin(mids)]
 
-ulabs = np.array(labels.label.value_counts().index)
-Y     = np.column_stack([labels.id.isin(labels.id[labels.label == l]).values for l in ulabs])
+# !! Doing the mapping correctly?
+# mids[i] is the id corresponding to the i'th row of all_encs
+id2pos        = dict(zip(mids, range(len(mids))))
+labels['pos'] = labels.id.apply(id2pos.get)
 
-XX = all_encs[labels.id.values]
+ulabs = np.unique(labels.label.values)
+upos  = labels.pos.drop_duplicates().sort_values().reset_index(drop=True)
+Y     = np.column_stack([upos.isin(labels.pos[labels.label == l]).values for l in ulabs])
 
-XX_train, XX_valid, Y_train, Y_valid = train_test_split(XX, Y, train_size=0.1)
-classifier = RandomForestClassifier(n_estimators=100, n_jobs=60, verbose=1).fit(XX_train, Y_train)
-preds = classifier.predict(XX_valid)
+XX = all_encs[upos.values]
+
+XX_train, XX_valid, Y_train, Y_valid = train_test_split(XX, Y, train_size=0.05)
+classifier = RandomForestClassifier(n_estimators=1024, n_jobs=60, verbose=1).fit(XX_train, Y_train)
+
+# >>
+# preds = classifier.predict(XX_valid)
+# --
+# !! How graphvite does prediction -- IMO this is sketchy
+scores = np.column_stack([p[:,1] for p in classifier.predict_proba(XX_valid)])
+thresh = np.sort(scores, axis=-1)[(np.arange(scores.shape[0]), -Y_valid.sum(axis=-1))]
+preds  = (scores >= thresh.reshape(-1, 1))
+# <<
+
 metrics.f1_score(Y_valid, preds, average='micro')
 metrics.f1_score(Y_valid, preds, average='macro')
 
+# 40 epochs (5%)
+# 0.3260745874928878
+# 0.20069794610611497
+
+# .. more epochs ..
+# 0.41138600581887214
+# 0.3038943278914481
+
+# >>
+
+from tqdm import trange
+from sklearn.svm import LinearSVC
+
+scores = [LinearSVC().fit(XX_train, Y_train[:,i]).decision_function(XX_valid) for i in trange(Y_train.shape[1])]
+scores = np.column_stack(scores)
+thresh = np.sort(scores, axis=-1)[(np.arange(scores.shape[0]), -Y_valid.sum(axis=-1))]
+preds  = (scores >= thresh.reshape(-1, 1))
+f1_micro = metrics.f1_score(Y_valid, preds, average='micro')
+f1_macro = metrics.f1_score(Y_valid, preds, average='macro')
+
+f1_micro
+f1_macro
+# <<
--- utils/dump.py
+++ utils/dump.py
@@ -8,11 +8,12 @@ import numpy as np
 import pandas as pd
 from scipy import sparse
 
+np.random.seed(111999)
+
 inpath = '/home/bjohnson/.graphvite/dataset/youtube/youtube_graph.txt'
 
 edges = pd.read_csv(inpath, header=None, sep='\t').values
 
-edges -= edges.min()
 n_nodes = edges.max() + 1
 
 vals = np.ones(edges.shape[0])
@@ -21,9 +22,10 @@ adj  = sparse.csr_matrix((vals, (edges[:,0], edges[:,1])), shape=(n_nodes, n_nod
 # Do we want to treat this as directory or undirected?
 adj  = ((adj + adj.T) > 0).astype(np.float32)
 
-# keep = np.where(adj @ np.ones(adj.shape[0]) != 0)[0]
-# adj  = adj[keep]
-# adj  = adj[:,keep]
+keep = np.where(adj @ np.ones(adj.shape[0]) != 0)[0]
+keep = np.random.permutation(keep)
+adj  = adj[keep]
+adj  = adj[:,keep]
 
 adj.sort_indices()
 adj.eliminate_zeros()
@@ -31,6 +33,4 @@ adj.eliminate_zeros()
 np.save('data/youtube/adj_data.npy', adj.data)
 np.save('data/youtube/adj_indices.npy', adj.indices)
 np.save('data/youtube/adj_indptr.npy', adj.indptr)
-# np.save('adj_ids.npy', keep)
-
-
+np.save('data/youtube/adj_ids.npy', keep)
